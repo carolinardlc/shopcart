1798291aa5622a7a4c94092b2bf25560f7fdb318
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2025-03-02 13:54:12 -0500

    Added new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '../components/ui/button';
+
 const Home = ()=>{
   return (
   <div className="p-10 bg-shop-light-pink">
     <h2 className="text-xl font-semibold">Home</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <Button size="lg">Check out</Button>
   </div>
   );
 };
