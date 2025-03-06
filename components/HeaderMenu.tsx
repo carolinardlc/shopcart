4b8e34f49e1214b3d403f2f988c7526e74f72e4e
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2025-03-06 11:26:46 -0500

    Updated HeaderMenu

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 const HeaderMenu = () => {
   return (
-    <div>
+    <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor'>
       {headerData?.map((item)=>(
-        <a key={item?.title} href={item?.href}>
+        <a key={item?.title} href={item?.href} className={`hover:text-shop_light_green hoverEffect relative group`}>
           {item?.title}
+          <span className={`absolute -bottom-0.5 right-1/2 w-1/2 h-0.5 bg-shop_light_green`}/>
         </a>
       ))}
     </div>
