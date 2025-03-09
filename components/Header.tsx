c88e1402f7ffbfbd06cfc68d389cd4e9c376e949
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2025-03-08 20:43:26 -0500

    Added a Search Bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,17 @@ import Container from '@/components/Container';
 import Logo from '@/components/Logo';
 import React from 'react';
 import HeaderMenu from './HeaderMenu';
+import SearchBar from './SearchBar';
 
 const Header = () => {
   return (
     <header className='bg-white py-5 border-b border-b-black/20'>
       <Container className='flex items-center justify-between'>
-        <Logo/>       
+        <Logo />
       <HeaderMenu />
-    <div>Others</div>
+    <div>
+      <SearchBar />
+    </div>
       </Container>
     </header>
   )
