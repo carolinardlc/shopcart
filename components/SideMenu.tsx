6601a8a1671331c821a9eeb249f694a94fb3c924
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2025-03-13 16:28:31 -0500

    Updated SideMenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react';
 import React, {FC} from "react";
 import Logo from "./Logo";
 interface SideBarProps {
@@ -9,15 +10,18 @@ const SideMenu: FC<SideBarProps>= ({isOpen, onClose}) => {
   return (
     <div
     className={`fixed inset-y-0 h-screen left-0 z-50 w-full
-    bg-black/50 shadow-xl ${
+    bg-black/50 text-white/80 shadow-xl ${
       isOpen ? "translate-x-0":"-translate-x-full"  
     } hoverEffect`}
     >
       <div className="min-w-72 max-w-96 bg-black h-screen
       p-10 border-r border-r-shop_light_green flex flex-col gap-6">
-        <div>
+        <div className='flex items-center justify-between gap-5'>
           <Logo className="text-white"/>
-          <button></button>
+          <button onClick={onClose} 
+          className='hover:text-shop_light_green hoverEffect'>
+            <X />
+          </button>
         </div>
         <div></div>
       </div>
