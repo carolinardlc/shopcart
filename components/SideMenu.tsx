67c8d46c6fe4bb04dcbd9f62c2d80324064de51c
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2025-03-14 17:46:00 -0500

    Updated Logo and SideMenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -17,7 +17,7 @@ const SideMenu: FC<SideBarProps>= ({isOpen, onClose}) => {
       <div className="min-w-72 max-w-96 bg-black h-screen
       p-10 border-r border-r-shop_light_green flex flex-col gap-6">
         <div className='flex items-center justify-between gap-5'>
-          <Logo className="text-white"/>
+          <Logo className="text-white" spanDesign="group-hover:text-white"/>
           <button onClick={onClose} 
           className='hover:text-shop_light_green hoverEffect'>
             <X />
