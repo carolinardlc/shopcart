c49054c14cbfad0e108ff9b9fec421461bd8ede4
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2025-03-15 18:21:32 -0500

    Updated SideMenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -3,16 +3,18 @@ import React, {FC} from "react";
 import Link from 'next/link';
 import Logo from "./Logo";
 import { headerData } from '@/constants/data';
+import { usePathname } from 'next/navigation';
 interface SideBarProps {
     isOpen: boolean;
     onClose: () => void;
 }
 
 const SideMenu: FC<SideBarProps>= ({isOpen, onClose}) => {
+    const pathname = usePathname();
   return (
     <div
     className={`fixed inset-y-0 h-screen left-0 z-50 w-full
-    bg-black/50 text-white/80 shadow-xl ${
+    bg-black/50 text-white/70 shadow-xl ${
       isOpen ? "translate-x-0":"-translate-x-full"  
     } hoverEffect`}
     >
@@ -30,7 +32,9 @@ const SideMenu: FC<SideBarProps>= ({isOpen, onClose}) => {
         font-semibold tracking-wide'>
           {headerData?.map((item)=>(
         <Link href={item?.href} key={item?.title}
-        className='hover:text-shop_light_green hoverEffect'>
+        className={`hover:text-shop_light_green hoverEffect ${pathname === item?.href &&
+          "text-white"}`}
+          >
           {item?.title}
         </Link>
           ))}
